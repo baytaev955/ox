16f10467d6dc34fd1ef541b4d41cbb69c1190089
baytaev955 <105934478+baytaev955@users.noreply.github.com>
2022-05-20 19:12:44 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:true,
+    //    adaptiveHeight:true,
+    slidesToShow:2,
+    slidesToScroll:2,
+    infinite:false
+    });
+});
+
+$(document).ready(function(){
+    $('.slider2').slick({
+        arrows:true,
+    //    adaptiveHeight:true,
+    slidesToShow:2,
+    slidesToScroll:2,
+    infinite:false
+    });
+});
